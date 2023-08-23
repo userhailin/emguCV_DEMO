1076dab5859b81a529eb93f9181b5649d993863d
userhailin <130354129+userhailin@users.noreply.github.com>
2023-08-23 16:04:00 +0800

    Add files via upload

---
+++ EmguCV.demo5/EmguCV.demo5/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace EmguCV.demo5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.LoadImage = new System.Windows.Forms.Button();
+            this.FormColor = new System.Windows.Forms.Button();
+            this.ScreenCapture = new System.Windows.Forms.Button();
+            this.WinTitleBox = new System.Windows.Forms.TextBox();
+            this.WindowTitle = new System.Windows.Forms.Button();
+            this.GrayCheck = new System.Windows.Forms.Button();
+            this.ImageCapture = new System.Windows.Forms.Button();
+            this.GrayConvert = new System.Windows.Forms.Button();
+            this.canny = new System.Windows.Forms.Button();
+            this.Equalization = new System.Windows.Forms.Button();
+            this.GaussianBlur = new System.Windows.Forms.Button();
+            this.MeanFilter = new System.Windows.Forms.Button();
+            this.HoughCircles = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(59, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(737, 473);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // LoadImage
+            // 
+            this.LoadImage.Location = new System.Drawing.Point(416, 491);
+            this.LoadImage.Name = "LoadImage";
+            this.LoadImage.Size = new System.Drawing.Size(88, 36);
+            this.LoadImage.TabIndex = 3;
+            this.LoadImage.Text = "加载图像";
+            this.LoadImage.UseVisualStyleBackColor = true;
+            this.LoadImage.Click += new System.EventHandler(this.LoadImage_Click);
+            // 
+            // FormColor
+            // 
+            this.FormColor.Location = new System.Drawing.Point(510, 491);
+            this.FormColor.Name = "FormColor";
+            this.FormColor.Size = new System.Drawing.Size(86, 36);
+            this.FormColor.TabIndex = 4;
+            this.FormColor.Text = "窗口变色";
+            this.FormColor.UseVisualStyleBackColor = true;
+            this.FormColor.Click += new System.EventHandler(this.FormColor_Click);
+            // 
+            // ScreenCapture
+            // 
+            this.ScreenCapture.Location = new System.Drawing.Point(190, 491);
+            this.ScreenCapture.Name = "ScreenCapture";
+            this.ScreenCapture.Size = new System.Drawing.Size(107, 36);
+            this.ScreenCapture.TabIndex = 5;
+            this.ScreenCapture.Text = "窗口捕获";
+            this.ScreenCapture.UseVisualStyleBackColor = true;
+            this.ScreenCapture.Click += new System.EventHandler(this.ScreenCapture_Click);
+            // 
+            // WinTitleBox
+            // 
+            this.WinTitleBox.Location = new System.Drawing.Point(802, 56);
+            this.WinTitleBox.Multiline = true;
+            this.WinTitleBox.Name = "WinTitleBox";
+            this.WinTitleBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.WinTitleBox.Size = new System.Drawing.Size(333, 390);
+            this.WinTitleBox.TabIndex = 6;
+            // 
+            // WindowTitle
+            // 
+            this.WindowTitle.Location = new System.Drawing.Point(68, 491);
+            this.WindowTitle.Name = "WindowTitle";
+            this.WindowTitle.Size = new System.Drawing.Size(116, 36);
+            this.WindowTitle.TabIndex = 7;
+            this.WindowTitle.Text = "窗口标题";
+            this.WindowTitle.UseVisualStyleBackColor = true;
+            this.WindowTitle.Click += new System.EventHandler(this.WindowTitle_Click);
+            // 
+            // GrayCheck
+            // 
+            this.GrayCheck.Location = new System.Drawing.Point(303, 492);
+            this.GrayCheck.Name = "GrayCheck";
+            this.GrayCheck.Size = new System.Drawing.Size(107, 35);
+            this.GrayCheck.TabIndex = 8;
+            this.GrayCheck.Text = "灰度判断";
+            this.GrayCheck.UseVisualStyleBackColor = true;
+            this.GrayCheck.Click += new System.EventHandler(this.GrayCheck_Click);
+            // 
+            // ImageCapture
+            // 
+            this.ImageCapture.Location = new System.Drawing.Point(602, 492);
+            this.ImageCapture.Name = "ImageCapture";
+            this.ImageCapture.Size = new System.Drawing.Size(106, 35);
+            this.ImageCapture.TabIndex = 10;
+            this.ImageCapture.Text = "开启摄像头";
+            this.ImageCapture.UseVisualStyleBackColor = true;
+            this.ImageCapture.Click += new System.EventHandler(this.ImageCapture_Click);
+            // 
+            // GrayConvert
+            // 
+            this.GrayConvert.Location = new System.Drawing.Point(68, 534);
+            this.GrayConvert.Name = "GrayConvert";
+            this.GrayConvert.Size = new System.Drawing.Size(116, 30);
+            this.GrayConvert.TabIndex = 11;
+            this.GrayConvert.Text = "灰度转换";
+            this.GrayConvert.UseVisualStyleBackColor = true;
+            this.GrayConvert.Click += new System.EventHandler(this.GrayConvert_Click);
+            // 
+            // canny
+            // 
+            this.canny.Location = new System.Drawing.Point(191, 534);
+            this.canny.Name = "canny";
+            this.canny.Size = new System.Drawing.Size(106, 30);
+            this.canny.TabIndex = 12;
+            this.canny.Text = "canny算子";
+            this.canny.UseVisualStyleBackColor = true;
+            this.canny.Click += new System.EventHandler(this.canny_Click);
+            // 
+            // Equalization
+            // 
+            this.Equalization.Location = new System.Drawing.Point(304, 534);
+            this.Equalization.Name = "Equalization";
+            this.Equalization.Size = new System.Drawing.Size(106, 30);
+            this.Equalization.TabIndex = 13;
+            this.Equalization.Text = "直方图均方化";
+            this.Equalization.UseVisualStyleBackColor = true;
+            this.Equalization.Click += new System.EventHandler(this.Equalization_Click);
+            // 
+            // GaussianBlur
+            // 
+            this.GaussianBlur.Location = new System.Drawing.Point(416, 534);
+            this.GaussianBlur.Name = "GaussianBlur";
+            this.GaussianBlur.Size = new System.Drawing.Size(88, 30);
+            this.GaussianBlur.TabIndex = 14;
+            this.GaussianBlur.Text = "高斯滤波";
+            this.GaussianBlur.UseVisualStyleBackColor = true;
+            this.GaussianBlur.Click += new System.EventHandler(this.GaussianBlur_Click);
+            // 
+            // MeanFilter
+            // 
+            this.MeanFilter.Location = new System.Drawing.Point(511, 534);
+            this.MeanFilter.Name = "MeanFilter";
+            this.MeanFilter.Size = new System.Drawing.Size(85, 30);
+            this.MeanFilter.TabIndex = 15;
+            this.MeanFilter.Text = "均值滤波";
+            this.MeanFilter.UseVisualStyleBackColor = true;
+            this.MeanFilter.Click += new System.EventHandler(this.MeanFilter_Click);
+            // 
+            // HoughCircles
+            // 
+            this.HoughCircles.Location = new System.Drawing.Point(603, 534);
+            this.HoughCircles.Name = "HoughCircles";
+            this.HoughCircles.Size = new System.Drawing.Size(105, 30);
+            this.HoughCircles.TabIndex = 16;
+            this.HoughCircles.Text = "霍夫圆检测";
+            this.HoughCircles.UseVisualStyleBackColor = true;
+            this.HoughCircles.Click += new System.EventHandler(this.HoughCircles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 597);
+            this.Controls.Add(this.HoughCircles);
+            this.Controls.Add(this.MeanFilter);
+            this.Controls.Add(this.GaussianBlur);
+            this.Controls.Add(this.Equalization);
+            this.Controls.Add(this.canny);
+            this.Controls.Add(this.GrayConvert);
+            this.Controls.Add(this.ImageCapture);
+            this.Controls.Add(this.GrayCheck);
+            this.Controls.Add(this.WindowTitle);
+            this.Controls.Add(this.WinTitleBox);
+            this.Controls.Add(this.ScreenCapture);
+            this.Controls.Add(this.FormColor);
+            this.Controls.Add(this.LoadImage);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "emguCV图像";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button LoadImage;
+        private System.Windows.Forms.Button FormColor;
+        private System.Windows.Forms.Button ScreenCapture;
+        private System.Windows.Forms.TextBox WinTitleBox;
+        private System.Windows.Forms.Button WindowTitle;
+        private System.Windows.Forms.Button GrayCheck;
+        private System.Windows.Forms.Button ImageCapture;
+        private System.Windows.Forms.Button GrayConvert;
+        private System.Windows.Forms.Button canny;
+        private System.Windows.Forms.Button Equalization;
+        private System.Windows.Forms.Button GaussianBlur;
+        private System.Windows.Forms.Button MeanFilter;
+        private System.Windows.Forms.Button HoughCircles;
+    }
+}
+
---
+++ EmguCV.demo5/EmguCV.demo5/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+using System.Runtime.InteropServices;
+
+namespace EmguCV.demo5
+{
+    public partial class Form1 : Form
+    {
+        [STAThread]
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, out RECT rect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight,
+            IntPtr hdcSrc, int nXSrc, int nYSrc, CopyPixelOperation dwRop);
+
+        private const int SRCCOPY = 0x00CC0020;
+        public Form1()
+        {
+            InitializeComponent();
+            //Application.Idle += ImageCapture_Click;
+        }
+
+        Image<Bgr, byte> scaledImage;
+        private void LoadImage_Click(object sender, EventArgs e)
+        {
+            var dialog=new OpenFileDialog();
+            //文件筛选器，结构：显示名称|文件类型
+            dialog.Filter = "图片(*.jpg/*.png/*.gif/*/bmp)|*.jpg;*.png;*.gif;*.bmp";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                var filename=dialog.FileName;
+                Image<Bgr, byte> image;
+                image = new Image<Bgr, byte>(@filename);
+                // 获取 ImageBox 控件的尺寸
+                int imageBoxWidth = imageBox1.Width;
+                int imageBoxHeight = imageBox1.Height;
+
+                // 获取图像的尺寸
+                int imageWidth = image.Width;
+                int imageHeight = image.Height;
+
+                // 计算缩放比例
+                double scaleWidth = (double)imageBoxWidth / imageWidth;
+                double scaleHeight = (double)imageBoxHeight / imageHeight;
+                double scaleFactor = Math.Min(scaleWidth, scaleHeight);
+
+                // 缩放图像,为了完整显示整个图像
+                scaledImage = image.Resize(scaleFactor, Inter.Linear);
+                imageBox1.Image = scaledImage;
+                
+            }
+        }
+
+        private void FormColor_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> img = new Image<Bgr, byte>(640, 400, new Bgr(200, 200, 0));
+            imageBox1.Image=img;
+        }
+
+        private void ScreenCapture_Click(object sender, EventArgs e)
+        {
+            string targetWindowTitle = "Visual studio 2010配置OpenCV环境.docx - Word"; // 指定目标窗口标题
+            //FindWindow函数查找具有类名为 "TXGuiFoundation" 的窗口。IntPtr 是一个指向内存中一个整数的指针，它在这里用于存储窗口的句柄。
+            //IntPtr targetWindowHandle = FindWindow("TXGuiFoundation", null);//第一个参数是类名，第二个参数是窗口标题名
+            IntPtr targetWindowHandle = FindWindow(null, targetWindowTitle);//第一个参数是类名，第二个参数是窗口标题名
+
+            if (targetWindowHandle != IntPtr.Zero)
+            {
+                RECT windowRect;
+                GetWindowRect(targetWindowHandle, out windowRect);//out表示windowRect是输出参数
+
+                int width = windowRect.Right - windowRect.Left;
+                int height = windowRect.Bottom - windowRect.Top;
+
+                using (Bitmap bitmap = new Bitmap(width, height))
+                {
+                    using (Graphics graphics = Graphics.FromImage(bitmap))
+                    {
+                        // IntPtr hdcDest = graphics.GetHdc();
+                        //IntPtr hdcSrc = GetWindowDC(targetWindowHandle);
+
+                        // 使用 BitBlt 将窗口内容复制到位图
+                        // BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, CopyPixelOperation.SourceInvert);
+
+                        // ReleaseDC(targetWindowHandle, hdcSrc);
+                        //graphics.ReleaseHdc(hdcDest);
+                        IntPtr hdc = graphics.GetHdc();
+                        PrintWindow(targetWindowHandle, hdc, 0);
+                        graphics.ReleaseHdc(hdc);
+                    }
+
+                    bitmap.Save("C:\\Users\\dhl\\Pictures\\测试图片\\123.png");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Target window not found.");
+            }
+
+        }
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+        
+
+        private void WindowTitle_Click(object sender, EventArgs e)
+        {
+            bool EnumWindowCallback(IntPtr hWnd, IntPtr lParam)
+            {
+                const int maxWindowTextLength = 256;
+                System.Text.StringBuilder windowText = new System.Text.StringBuilder(maxWindowTextLength);
+                int length = GetWindowText(hWnd, windowText, maxWindowTextLength);
+
+                if (length > 0)
+                {
+                    string windowTitle = windowText.ToString();
+                    WinTitleBox.AppendText("Window Title: " + windowTitle + Environment.NewLine + "------------------------" + Environment.NewLine);
+                }
+
+                return true; // 继续遍历其他窗口
+            }
+            EnumWindows(EnumWindowCallback, IntPtr.Zero);
+        }
+
+        private void GrayCheck_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> grayImage = scaledImage.Convert<Gray, byte>();
+            double thresholdValue = 20;
+            // 创建一个输出图像，用于存储阈值处理后的结果
+            Image<Gray, byte> thresholdedImage = new Image<Gray, byte>(grayImage.Width, grayImage.Height);
+
+            // 应用阈值处理
+            CvInvoke.Threshold(grayImage, thresholdedImage, thresholdValue, 255, ThresholdType.Binary);
+
+            // 统计非零像素的数量
+            int nonZeroPixelCount = CvInvoke.CountNonZero(thresholdedImage);
+            MessageBox.Show(nonZeroPixelCount.ToString());
+
+            // 判断是否全黑，0表示黑色，255则表示白色
+            bool isAllBlack = (nonZeroPixelCount <= 1000);
+
+            MessageBox.Show("Is the image all black? " + isAllBlack);
+
+            // CvInvoke.Imshow("Thresholded Image", thresholdedImage);
+
+            imageBox1.Image = thresholdedImage;
+        }
+
+        private void ImageCapture_Click(object sender, EventArgs e)
+        {
+            VideoCapture capture = new VideoCapture();
+            //imageBox1.Image = capture.QueryFrame();
+            Application.Idle += new EventHandler(delegate (object s, EventArgs ea)
+            {
+                imageBox1.Image = capture.QueryFrame();
+            });
+        }
+        private Image<Bgr, byte> ProcessImage(Image<Bgr, byte> inputFrame)
+        {
+            // 在这里进行图像处理，例如滤波、边缘检测等
+            return inputFrame; // 返回处理后的图像
+        }
+       
+        private void GrayConvert_Click(object sender, EventArgs e)
+        {
+            //CvInvoke.Imshow("Jinx", scaledImage);
+            Mat grayImage = new Mat();
+            //转为灰度图像
+            CvInvoke.CvtColor(scaledImage, grayImage, ColorConversion.Rgb2Gray);
+            CvInvoke.Imshow("Gray Image", grayImage);
+        }
+
+        private void canny_Click(object sender, EventArgs e)
+        {
+            Mat grayImage = new Mat();
+            //转为灰度图像
+            CvInvoke.CvtColor(scaledImage, grayImage, ColorConversion.Rgb2Gray);
+            CvInvoke.GaussianBlur(grayImage, grayImage, new Size(3, 3), 3);
+            Mat cannyImg = new Mat();
+            CvInvoke.Canny(grayImage, cannyImg, 20, 40);
+            CvInvoke.Imshow("Canny Image", cannyImg);
+        }
+
+        private void Equalization_Click(object sender, EventArgs e)
+        {
+            Mat grayImage = new Mat();
+            //转为灰度图像
+            CvInvoke.CvtColor(scaledImage, grayImage, ColorConversion.Rgb2Gray);
+            Mat equalizationImage= new Mat(scaledImage.Size, DepthType.Cv8U, 1);
+            CvInvoke.EqualizeHist(grayImage, equalizationImage);
+            CvInvoke.Imshow("Equalization", equalizationImage);
+        }
+
+        private void GaussianBlur_Click(object sender, EventArgs e)
+        {
+            Mat grayImage = new Mat();
+            //转为灰度图像
+            CvInvoke.CvtColor(scaledImage, grayImage, ColorConversion.Rgb2Gray);
+            CvInvoke.GaussianBlur(grayImage, grayImage, new Size(3, 3), 3);
+            CvInvoke.Imshow("GaussianBlur Image", grayImage);
+        }
+
+        private void MeanFilter_Click(object sender, EventArgs e)
+        {
+            Mat grayImage = new Mat();
+            //转为灰度图像
+            CvInvoke.CvtColor(scaledImage, grayImage, ColorConversion.Rgb2Gray);
+            CvInvoke.Blur(grayImage, grayImage, new Size(3, 3), new Point(-1, -1));
+            CvInvoke.Imshow("MeanFilter Image", grayImage);
+        }
+
+        private void HoughCircles_Click(object sender, EventArgs e)
+        {
+            Mat grayImage = new Mat();
+            //转为灰度图像
+            CvInvoke.CvtColor(scaledImage, grayImage, ColorConversion.Rgb2Gray);
+            //使用高斯滤波去除噪声
+            CvInvoke.GaussianBlur(grayImage, grayImage, new Size(5, 5), 3);
+            //霍夫圆检测
+            CircleF[] circles = CvInvoke.HoughCircles(grayImage, HoughType.Gradient, 2.0, 20.0, 100.0, 180.0, 5);
+            #region draw circles
+            Image<Bgr, Byte> circleImage = scaledImage.Clone();
+            foreach (CircleF circle in circles)
+                circleImage.Draw(circle, new Bgr(Color.Blue), 4);
+            CvInvoke.Imshow("HoughCircles", circleImage);
+            #endregion
+        }
+    }
+}
